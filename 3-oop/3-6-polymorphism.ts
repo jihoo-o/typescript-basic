04ead5b87a254fe1c7d2e21d761a5c4cb1a66e14
kmseonhwa <seonhwakei@gmail.com>
2022-02-01 18:34:48 +0900

    Add: polymorphism

---
+++ 3-oop/3-6-polymorphism.ts
@@ -0,0 +1,110 @@
+{
+    type CoffeeCup = {
+        shots: number;
+        hasMilk?: boolean;
+        hasSugar?: boolean;
+    };
+
+    interface CoffeeMaker {
+        makeCoffee(shots: number): CoffeeCup;
+    }
+
+    class CoffeeMachine implements CoffeeMaker {
+        private static BEANS_GRAM_PER_SHOT: number = 7; // class level
+        private coffeeBeans: number = 0; // instance (object) level
+
+        constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMachine {
+            return new CoffeeMachine(coffeeBeans);
+        }
+
+        fillCoffeeBeans(beans: number) {
+            if (beans < 0) {
+                throw new Error('value for beans should be greater than 0');
+            } else {
+                this.coffeeBeans += beans;
+            }
+        }
+
+        clean() {
+            console.log('cleaning the machine...🧼');
+        }
+
+        private grindBeans(shots: number) {
+            console.log(`grinding beans for ${shots}`);
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+                throw new Error('Not enough coffee beans!');
+            }
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+        }
+
+        private preheat() {
+            console.log('heatingi up... 🔥');
+        }
+
+        private extract(shots: number): CoffeeCup {
+            console.log(`Pulling ${shots} shots...☕️`);
+            return {
+                shots,
+                hasMilk: false,
+            };
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots);
+        }
+    }
+
+    class CaffeLatteMachine extends CoffeeMachine {
+        constructor(beans: number, public readonly serialNumber: string) {
+            super(beans);
+        }
+
+        private steamMilk() {
+            console.log('Steaming some milk...🥛');
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            this.steamMilk();
+            return {
+                ...coffee,
+                hasMilk: true,
+            };
+        }
+    }
+
+    class SweetCoffeeMaker extends CoffeeMachine {
+        private addSugar() {
+            console.log('add sugar 🍬');
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            this.addSugar();
+            return {
+                ...coffee,
+                hasSugar: true,
+            };
+        }
+    }
+
+    const machines: CoffeeMaker[] = [
+        new CoffeeMachine(16),
+        new CaffeLatteMachine(16, 'serial number'),
+        new SweetCoffeeMaker(16),
+        new CoffeeMachine(16),
+        new CaffeLatteMachine(16, 'serial number'),
+        new SweetCoffeeMaker(16),
+    ];
+
+    machines.forEach((machine) => {
+        console.log('--------------------');
+        machine.makeCoffee(1);
+    });
+}
