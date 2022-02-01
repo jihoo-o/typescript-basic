d1ce370a63ce9713c27b95c671ca1d207d8c6149
kmseonhwa <seonhwakei@gmail.com>
2022-02-01 12:17:18 +0900

    Add: loading

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -1,26 +1,43 @@
 {
-  /**
-   * Print Loading State
-   */
-  type LoadingState = {
-    state: 'loading';
-  };
-
-  type SuccessState = {
-    state: 'success';
-    response: {
-      body: string;
+    /**
+     * Print Loading State
+     */
+    type LoadingState = {
+        state: 'loading';
     };
-  };
 
-  type FailState = {
-    state: 'fail';
-    reason: string;
-  };
+    type SuccessState = {
+        state: 'success';
+        response: {
+            body: string;
+        };
+    };
 
-  type ResourceLoadState = LoadingState | SuccessState | FailState;
+    type FailState = {
+        state: 'fail';
+        reason: string;
+    };
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+    type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+    // state에 맞게 메시지 표시하기
+    function printLoginState(state: ResourceLoadState) {
+        switch (state.state) {
+            case 'success':
+                console.log(`😃 ${state.response.body}`);
+                return;
+            case 'fail':
+                console.log(`😱 ${state.reason}`);
+                return;
+            case 'loading':
+                console.log('👀 loading...');
+                return;
+            default:
+                throw new Error(`unknown state: ${state}`);
+        }
+    }
+
+    printLoginState({ state: 'loading' }); // 👀 loading...
+    printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+    printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
 }
