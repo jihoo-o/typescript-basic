3609660847009806dee08ae149e264a6bf8e7a48
kmseonhwa <seonhwakei@gmail.com>
2022-02-01 12:09:40 +0900

    Add: game

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,36 @@
 /**
  * Let's make a game 🕹
  */
+
+type Position = {
+    x: number;
+    y: number;
+};
+const position: Position = {
+    x: 0,
+    y: 0,
+};
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+function move(direction: Direction) {
+    switch (direction) {
+        case 'up':
+            position.y += 1;
+            return;
+        case 'down':
+            position.y -= 1;
+            return;
+        case 'left':
+            position.x -= 1;
+            return;
+        case 'right':
+            position.x += 1;
+            return;
+        default:
+            throw new Error(`unknown direction!: ${direction}`);
+    }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
