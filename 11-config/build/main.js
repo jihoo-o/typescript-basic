18c65547e8023a09970ba50f4c15286fae58de01
kmseonhwa <seonhwakei@gmail.com>
2022-02-03 16:51:56 +0900

    Add: 디버깅

--- 11-config/build/main.js
+++ 11-config/build/main.js
@@ -1,11 +1,14 @@
+"use strict";
 class Car {
-    engine = 0;
+    constructor() {
+        this.engine = 0;
+    }
     move() {
         const engine = this.engine + 1;
         console.log('engine 🐛');
         console.log(engine);
     }
 }
-
 const car = new Car();
 car.move();
+//# sourceMappingURL=main.js.map
