2b861742e44454ba7925f5afedb1f4fe140a43a3
kmseonhwa <seonhwakei@gmail.com>
2022-02-03 11:42:57 +0900

    Add: omit type

---
+++ 9-advanced/9-8-omit.ts
@@ -0,0 +1,27 @@
+{
+    type Video = {
+        id: string;
+        title: string;
+        url: string;
+        data: string;
+    };
+
+    function getVideo(id: string): Video {
+        return {
+            id,
+            title: 'video',
+            url: 'https://...',
+            data: 'byte-data..',
+        };
+    }
+
+    // ✨ Video 전체가 아닌 일부만 반환하는 경우
+    type VideoMetadata = Omit<Video, 'url' | 'data'>;
+
+    function getVideoMetadata(id: string): VideoMetadata {
+        return {
+            id,
+            title: 'title',
+        };
+    }
+}
