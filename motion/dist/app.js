67e35a86f08476d7041c9ae3dbef16d14e4fb3de
kmseonhwa <seonhwakei@gmail.com>
2022-02-05 16:06:37 +0900

    Add: ImageComponent

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,8 +1,11 @@
-import { PageComponent } from './components/page.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent } from './components/page/page.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attatchTo(appRoot);
+        const image = new ImageComponent('Image title', 'https://i.picsum.photos/id/516/200/300.jpg?hmac=hMEuvTcrLNhrMSSGnaRit4YgalzJJ66stNu-UT70DKw');
+        image.attatchTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,10 +1,18 @@
-import { PageComponent } from './components/page.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent } from './components/page/page.js';
 
 class App {
     private readonly page: PageComponent;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attatchTo(appRoot);
+
+        // test
+        const image = new ImageComponent(
+            'Image title',
+            'https://i.picsum.photos/id/516/200/300.jpg?hmac=hMEuvTcrLNhrMSSGnaRit4YgalzJJ66stNu-UT70DKw'
+        );
+        image.attatchTo(appRoot, 'beforeend');
     }
 }
 
