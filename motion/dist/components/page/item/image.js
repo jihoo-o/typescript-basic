973d4ea966529ecd12252637b6ec822dc4870e1f
kmseonhwa <seonhwakei@gmail.com>
2022-02-05 16:48:43 +0900

    Add: BaseComponent

--- motion/dist/components/page/item/image.js
+++ motion/dist/components/page/item/image.js
@@ -1,19 +1,14 @@
-export class ImageComponent {
+import { BaseComponent } from '../../component.js';
+export class ImageComponent extends BaseComponent {
     constructor(title, url) {
-        const template = document.createElement('template');
-        template.innerHTML = `
-            <section class="image">
-                <div class="image__holder"><img class="image__thumbnail"></div>
-                <p class="image__title">title</p>
-            </section>`;
-        this.element = template.content.firstElementChild;
+        super(`<section class="image">
+        <div class="image__holder"><img class="image__thumbnail"></div>
+        <p class="image__title">title</p>
+    </section>`);
         const imageElement = this.element.querySelector('.image__thumbnail');
         imageElement.src = url;
         imageElement.alt = title;
         const titleElement = this.element.querySelector('.image__title');
         titleElement.textContent = title;
-    }
-    attatchTo(parent, position = 'afterbegin') {
-        parent.insertAdjacentElement(position, this.element);
     }
 }
---
+++ motion/src/components/component.ts
@@ -0,0 +1,17 @@
+export interface Component {
+    attatchTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+
+export class BaseComponent<T extends HTMLElement> implements Component {
+    protected readonly element: T;
+
+    constructor(htmlString: string) {
+        const template = document.createElement('template');
+        template.innerHTML = htmlString;
+        this.element = template.content.firstElementChild! as T;
+    }
+
+    attatchTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+        parent.insertAdjacentElement(position, this.element);
+    }
+}
